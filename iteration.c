833727f384538f5bc8e0f6f3a536966ae03a0843
Darion de la Cruz <delacruzd9221@my.sduhsd.net>
2020-01-06 13:06:32 -0800

    new stuff

---
+++ iteration.c
@@ -0,0 +1,14 @@
+# include <stdio.h> 
+
+int main()
+{
+		int div=8;
+		printf("Div=8")
+		for(int i=1; i<==100; i++)
+		{
+			if(i%div==0)
+			{
+				printf"(%d\n" , i);
+			}
+		}
+}
---
+++ sizeofvariables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  int a = 545;
+  char b = 'x'
+  float c = 4.12
+  double d = 5.24421
+  // print value and size of an int variable
+  printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
+  printf("char b value: %d and size: %d bytes\n", a, sizeof(a));
+  printf("float c value: %d and size: %d bytes\n", a, sizeof(a));
+  printf("double d value: %d and size: %d bytes\n", a, sizeof(a));
+}
+
